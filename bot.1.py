eb977f8790b20ff0c13326140cb56871e06b7e90
Monstermike811 <miguelangelbersal@gmail.com>
2025-09-25 19:36:34 -0500

    Update bot.2.py

--- bot.1.py
+++ bot.1.py
@@ -1,5 +1,7 @@
 import discord
 import random
+import os
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -63,5 +65,52 @@ async def joined(ctx, member: discord.Member):
         await ctx.send(f'{member} has no join date.')
     else:
         await ctx.send(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def mem(ctx):
+    imagenes = os.listdir("imagenes")
+    with open(f'imagenes/{random.choice(imagenes)}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def animeme(ctx):
+    animemes = os.listdir("animemes")
+    with open(f'animemes/{random.choice(animemes)}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def poke(ctx,arg):
+    try:
+        pokemon = arg.split(" ",1)[0].lower()
+        result = requests.get("https://pokeapi.co/api/v2/pokemon/"+pokemon)
+        if result.text == "Not Found":
+            await ctx.send("Pokemon no encontrado")
+        else:
+            image_url = result.json()["sprites"]["front_default"]
+            print(image_url)
+            await ctx.send(image_url)
+
+    except Exception as e:
+        print("Error:", e)
+@poke.error
+async def error_type(ctx,error):
+    if isinstance(error,commands.errors.MissingRequiredArgument):
+        await ctx.send("Tienes que darme un pokemon")
 
 bot.run("token")
